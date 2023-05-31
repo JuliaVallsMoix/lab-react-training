3d0971800c438ca18256709a8122e58627683144
Júlia Valls Moix <juliavalls20@gmail.com>
2023-05-31 14:29:12 +0200

    FaceBook 14 to continue

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -3,25 +3,46 @@
 import profiles from '../data/berlin.json';
 import '../styles/FaceBook.css';
 
+// 1. Transformar el array de profiles a un array de solo paises
+// 2. Como hago para eliminar los repetidos de un array?
+// 3. Console.log con el array final a ver si de verdad estan solo paises y no hay repetidos
+
+const initialCountryList = profiles.map((profile) => profile.country);
+
+console.log(initialCountryList);
+
+const countryList = initialCountryList.filter((item, index) => initialCountryList.indexOf(item) === index);
+
+console.log(countryList);
+
 export default function FaceBook() {
-    const listProfiles = profiles.map(profile =>
-      <li>
-        <div className='img-container'>
-            <img
-            src={profile.img}
-            alt={profile.firstName}
-            />
+
+    const buttons = <div> {countryList.map((country) => <button> {country} </button>)} </div>
+
+    const listProfiles = profiles.map((profile) =>
+
+        <li>
+            <div className='img-container'>
+                <img
+                    src={profile.img}
+                    alt={profile.firstName}
+                />
+            </div>
+            <div className='profile-details'>
+                <p><strong>First Name:</strong> {profile.firstName}</p>
+                <p><strong>Last Name:</strong> {profile.lastName}</p>
+                <p><strong>Country:</strong> {profile.country}</p>
+                <p><strong>Type:</strong> {profile.isStudent ? 'Student' : 'Teacher'}</p>
+            </div>
+        </li>
+
+    );
+    return (
+
+        <div>
+            {buttons}
+            <ul> {listProfiles} </ul>
         </div>
 
-        <div className='profile-details'>
-            <p><strong>First Name:</strong> {profile.firstName}</p>
-            <p><strong>Last Name:</strong> {profile.lastName}</p>
-            <p><strong>Country:</strong> {profile.country}</p>
-            <p><strong>Type:</strong> {profile.isStudent ? 'Student' : 'Teacher'}</p>
-        </div>
-
-      </li>  
-      
-      );
-      return <ul> {listProfiles} </ul>
+    )
 }
